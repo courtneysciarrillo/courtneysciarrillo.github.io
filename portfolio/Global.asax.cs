eb592bd6fec46d4073943b499932cbad95020eac
courtneysciarrillo <courtney.sciarrillo@gmail.com>
2018-07-02 08:11:42 -0400

    commit

---
+++ portfolio/Global.asax.cs
@@ -0,0 +1,11 @@
+﻿using System.Web;
+
+namespace portfolio
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start()
+        {
+        }
+    }
+}
---
+++ portfolio/javascript.js
@@ -0,0 +1,17 @@
+﻿function ShowHide(div) {     d = document.getElementById(div);          $('.projects').bind('mousemove', function(e){             $('.tail').css({                 left:  e.pageX - 300,                 top:   e.pageY - 130             });         });      if( d.style.display == "none" )     {         d.style.display = "";     }     else     {         d.style.display = "none";     }  }
+
+function SwapDivsWithMouseOver(div1,div2)
+{
+    d1 = document.getElementById(div1);
+    d2 = document.getElementById(div2);
+   if( d2.style.display == "none" )
+   {
+        d1.style.display = "none";
+        d2.style.display = "";
+   }
+   else
+   {
+        d1.style.display = "";
+        d2.style.display = "none";
+   }
+}
