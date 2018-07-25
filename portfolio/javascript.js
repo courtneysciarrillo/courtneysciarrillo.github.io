37d2ca4cbf6b9828f36ec49d217afd2cd2e10d72
courtneysciarrillo <courtney.sciarrillo@gmail.com>
2018-07-25 04:29:47 -0400

    portfolio version 2

--- portfolio/javascript.js
+++ portfolio/javascript.js
@@ -1,17 +1,7 @@
-﻿function ShowHide(div) {     d = document.getElementById(div);          $('.projects').bind('mousemove', function(e){             $('.tail').css({                 left:  e.pageX - 300,                 top:   e.pageY - 130             });         });      if( d.style.display == "none" )     {         d.style.display = "";     }     else     {         d.style.display = "none";     }  }
-
-function SwapDivsWithMouseOver(div1,div2)
-{
-    d1 = document.getElementById(div1);
-    d2 = document.getElementById(div2);
-   if( d2.style.display == "none" )
-   {
-        d1.style.display = "none";
-        d2.style.display = "";
-   }
-   else
-   {
-        d1.style.display = "";
-        d2.style.display = "none";
-   }
-}
+﻿/*********************************************
+ * Uses jQuery!
+*********************************************/
+function myFunction(x) {
+  x.classList.toggle("change");
+  $("#mobile-menu").toggleClass("menu-hidden", 800, "easeOutQuint");
+};
